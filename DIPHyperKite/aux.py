9d8136e820b09810c72ec4622a7b0b6b2df266f8
matciotola <matciotola@gmail.com>
2024-01-19 14:54:50 +0100

    minor changes

--- DIPHyperKite/aux.py
+++ DIPHyperKite/aux.py
@@ -9,30 +9,20 @@ class TrainingDatasetKite(Dataset):
         super(TrainingDatasetKite, self).__init__()
 
         pan = []
-        #ms_lr = []
-        #ms = []
         gt = []
 
         for i in range(len(img_paths)):
             pan_single, _, _, gt_single, _ = open_mat(img_paths[i])
             pan.append(pan_single.float())
-            #ms_lr.append(ms_lr_single.float())
-            #ms.append(ms_single.float())
             gt.append(gt_single.float())
 
         pan = torch.cat(pan, 0)
-        #ms_lr = torch.cat(ms_lr, 0)
-        #ms = torch.cat(ms, 0)
         gt = torch.cat(gt, 0)
 
         pan = norm(pan)
-        #ms_lr = norm(ms_lr)
-        #ms = norm(ms)
         gt = norm(gt)
 
         self.pan = pan
-        #self.ms_lr = ms_lr
-        #self.ms = ms
         self.gt = gt
         self.priors = priors
 
