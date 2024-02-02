4faa93de0e4c03a6b22a01ee747d68f5d6a4d648
matciotola <matciotola@gmail.com>
2024-02-02 11:39:35 +0100

    bug fix

--- HSpeNet/loss.py
+++ HSpeNet/loss.py
@@ -14,13 +14,14 @@ class SAMLoss(nn.Module):
         norm_outputs = torch.sum(outputs * outputs, dim=1)
         norm_labels = torch.sum(labels * labels, dim=1)
         scalar_product = torch.sum(outputs * labels, dim=1)
-        norm_product = torch.sqrt(norm_outputs * norm_labels)
+        norm_product_sq = norm_outputs * norm_labels
 
-        mask = norm_product == 0
+        mask = norm_product_sq == 0
         scalar_product = mask * self.eps + torch.logical_not(mask) * scalar_product
-        norm_product = mask * self.eps + torch.logical_not(mask) * norm_product
+        norm_product_sq = mask * self.eps + torch.logical_not(mask) * norm_product_sq
+        norm_product = torch.sqrt(norm_product_sq)
         scalar_product = torch.flatten(scalar_product, 1, 2)
         norm_product = torch.flatten(norm_product, 1, 2)
-        angle = torch.sum(scalar_product / norm_product, dim=1) / norm_product.shape[1]
-        loss = 1 - angle
+        cosine = scalar_product / norm_product
+        loss = torch.sum(1 - cosine, dim=1) / norm_product.shape[1]
         return torch.mean(loss)
