437c6f73e7c47e10ebf31e4949e2e5dd0744c206
matciotola <matciotola@gmail.com>
2024-07-01 13:16:52 +0200

    delete testing code

--- CS/GS.py
+++ CS/GS.py
@@ -55,31 +55,3 @@ def GSA(ordered_dict):
     fused = V_hat - V_hat.mean(dim=(-2, -1), keepdim=True) + ms.mean(dim=(-2, -1), keepdim=True)
 
     return fused
-
-if __name__ == '__main__':
-    from scipy import io
-    import matplotlib
-    matplotlib.use('TkAgg')
-    from matplotlib import pyplot as plt
-    import numpy as np
-    from recordclass import recordclass
-    from Utils.interpolator_tools import interp23tap
-    temp = io.loadmat('/home/matteo/Desktop/Datasets/WV3_Adelaide_crops/Adelaide_1_zoom.mat')
-
-    ms_lr = temp['I_MS_LR'].astype(np.float64)
-    ms = interp23tap(ms_lr, 4)
-    pan = temp['I_PAN'].astype(np.float64)
-    ratio = 4
-
-    ms_lr = torch.tensor(np.moveaxis(ms_lr, -1, 0)[None, :, :, :])
-    ms = torch.tensor(np.moveaxis(ms, -1, 0)[None, :, :, :])
-    pan = torch.tensor(pan[None, None, :, :])
-
-    ord_dic = {'ms': ms, 'pan': pan, 'ms_lr': ms_lr, 'ratio': ratio}
-
-    exp_input = recordclass('exp_info', ord_dic.keys())(*ord_dic.values())
-
-    fused = GSA(exp_input)
-    plt.figure()
-    plt.imshow(fused[0, 0, :, :].detach().numpy(), cmap='gray')
-    plt.show()
