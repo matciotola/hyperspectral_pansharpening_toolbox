9192cfcf2dccb2f615b87b1a18355b8b34858b08
matciotola <matciotola@gmail.com>
2024-07-01 13:17:25 +0200

    weights load bug fix

--- HyperPNN/HyperPNN.py
+++ HyperPNN/HyperPNN.py
@@ -28,9 +28,8 @@ def HyperPNN(ordered_dict):
     net = HyperPNN_model(ms.shape[1])
 
     if not (config.train and ordered_dict.img_number == 0) or config.resume:
-        model_weights_path = os.path.join(os.path.dirname(inspect.getfile(HyperPNN_model)), 'weights', ordered_dict.dataset + '.tar')
         if not model_weights_path:
-            pass
+            model_weights_path = os.path.join(os.path.dirname(inspect.getfile(HyperPNN_model)), 'weights', ordered_dict.dataset + '.tar')
         if os.path.exists(model_weights_path):
             net.load_state_dict(torch.load(model_weights_path))
             print('Weights loaded from: ' + model_weights_path)
